03a7dc772ecac151eca07bcff69e24aa18c148b3
arshul <arshulmohd123@gmail.com>
2019-06-30 12:49:58 +0530

    update seissol executable path

--- workflows/containerized/scripts/execute.sh
+++ workflows/containerized/scripts/execute.sh
@@ -4,10 +4,10 @@ tar -xzf Examples.tar.gz
 
 pushd seissol
 mkdir -p launch_SeisSol
-cp -R build/build_release_generatedKernels_dnoarch_hybrid_none_9_6 launch_SeisSol/
+cp -R build/SeisSol_release_generatedKernels_dnoarch_hybrid_none_9_4 launch_SeisSol/
 cp ../Examples-master/tpv33/* launch_SeisSol
 echo $PWD/Maple/ > launch_SeisSol/DGPATH
 popd
 
 cd seissol/launch_SeisSol
-OMP_NUM_THREADS=1 mpiexec --allow-run-as-root -np 1 ./build_release_generatedKernels_dnoarch_hybrid_none_9_6  parameters_tpv33_master.par
+OMP_NUM_THREADS=1 mpiexec --allow-run-as-root -np 1 ./SeisSol_release_generatedKernels_dnoarch_hybrid_none_9_4 parameters_tpv33_master.par
