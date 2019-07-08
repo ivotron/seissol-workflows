b17a7d3faf9997d166c73225f666f5d0ed676dbb
Ivo Jimenez <ivo.jimenez@gmail.com>
2019-07-07 20:41:08 -0700

    Removes scc18 workflow files
    [skip ci]

--- workflows/scc18/scripts/execute.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p ./workflows/scc18/input-data/output
-cp seissol/build/SeisSol_release_generatedKernels_dnoarch_hybrid_none_9_6 ./workflows/scc18/input-data
-cd ./workflows/scc18/input-data
-mpiexec --allow-run-as-root --oversubscribe -np 1 ./SeisSol_release_generatedKernels_dnoarch_hybrid_none_9_6 parameters_zenodo_easi.par > output/out.txt
